159138046a8b5ddd6b744dbe825abe5d6155cad6
Yuri Kiev de Sousa Barreto <yurikievbarreto@gmail.com>
2026-06-27 20:04:01 -0300

    feat: update chat and message repositories

--- app/repositories/chat_repository.py
+++ app/repositories/chat_repository.py
@@ -1,5 +1,6 @@
 from app.models.chat_model import Chat, ChatCreate
-from sqlmodel import Session
+from sqlmodel import Session, select
+from sqlalchemy.orm import selectinload
 import uuid
 
 def create_chat(session: Session, chat_info: ChatCreate, user_id: uuid.UUID) -> Chat:
@@ -15,3 +16,20 @@ def create_chat(session: Session, chat_info: ChatCreate, user_id: uuid.UUID) -> 
 
 
     return new_chat
+
+
+def find_chats_by_user_id(session: Session, user_id: uuid.UUID) -> list[Chat]:
+    query = (
+        select(Chat)
+        .where(Chat.user_id == user_id)
+        .options(selectinload(Chat.messages))
+    )
+
+    chats = list(session.exec(query).all())
+    print(chats)
+
+    return chats
+
+def find_chat_by_id(session: Session, chat_id: uuid.UUID) -> Chat | None:
+    query = select(Chat).where(Chat.id == chat_id)
+    return session.exec(query).first()
--- app/repositories/message_repository.py
+++ app/repositories/message_repository.py
@@ -1,18 +1,20 @@
 from app.models.message_model import Message, MessageCreate
 from app.models.chat_model import Chat
 from sqlmodel import Session
+import uuid
 
 
-def create_message(session: Session, chat_info: ChatCreate, user_id: uuid.UUID) -> Chat:
-    new_chat = Chat(
-        title=chat_info.title,
-        user_id=user_id
+def create_message(session: Session, message_data: MessageCreate, chat_id: uuid.UUID) -> Message:
+    new_message = Message(
+       role=message_data.role,
+       content=message_data.content,
+       chat_id=chat_id
     )
 
     
-    session.add(new_chat)
+    session.add(new_message)
     session.commit()
-    session.refresh(new_chat)
+    session.refresh(new_message)
 
 
-    return new_chat
+    return new_message
