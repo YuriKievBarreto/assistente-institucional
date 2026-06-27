10f2c2a5b102c6d4205726d78faad6155ff85aaf
Yuri Kiev de Sousa Barreto <yurikievbarreto@gmail.com>
2026-06-27 19:57:19 -0300

    feat: add endpoint to list user chats with messages

--- app/api/v1/routes/chat_router.py
+++ app/api/v1/routes/chat_router.py
@@ -1,10 +1,12 @@
 from fastapi import APIRouter, Depends, Body, status, HTTPException
 from typing import List
 from app.models.user_model import User
-from app.models.chat_model import ChatMigrateRequest, ChatInputRequest
+from app.models.chat_model import ChatMigrateRequest, ChatInputRequest, AIresponse, ChatResponse, Dialogue
 from app.chatbot.engine import ChatEngine
-from app.dependencies import get_engine, get_current_user
+from app.dependencies import get_engine, get_current_user, get_current_user_optional
 from app.database.postgres import get_session
+from app.repositories.chat_repository import find_chats_by_user_id
+
 
 from sqlmodel import Session
 from app.api.services import chat_service
@@ -12,11 +14,30 @@ from app.api.services import chat_service
 chat_router = APIRouter()
 
 
-@chat_router.post("/")
-async def chat(req: ChatInputRequest, engine: ChatEngine = Depends(get_engine)):
-    print(req)
-    return chat_service.chat(engine, req)
+@chat_router.post("/", response_model=AIresponse)
+async def chat(req: ChatInputRequest, engine: ChatEngine = Depends(get_engine), 
+               current_user : User | None = Depends(get_current_user_optional), 
+               session: Session = Depends(get_session)):
+    
 
+    ai_response = chat_service.chat(engine, req)
+    if current_user:
+       print("usuario ativo encontrado")
+       print("craindo obj de dialogo")
+       dialogue_data = Dialogue(
+           title=req.title,
+           human_message=req.query,
+           AI_response=ai_response.answer,
+           user_id=current_user.id,
+           chat_id=req.session_id
+       )
+       print(dialogue_data)
+
+       chat_service.save_dialogue(session, dialogue_data)
+
+    print("nenhum usuario ativo encontrado!, nenhuma mensagem será salva na memoria")
+
+    return ai_response
 
 
 @chat_router.post("/migrate", status_code=status.HTTP_201_CREATED)
@@ -35,3 +56,9 @@ async def migrate(req: ChatMigrateRequest,
        )
 
 
+@chat_router.get("/", response_model=list[ChatResponse])
+async def get_all_chats(session: Session = Depends(get_session),
+                        current_user: User = Depends(get_current_user)):
+    
+    return find_chats_by_user_id(session, current_user.id)
+    
