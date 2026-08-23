77b55c1119f7fb89d1a09b5bf392b59d419aa24f
Yuri Kiev de Sousa Barreto <yurikievbarreto@gmail.com>
2026-08-23 19:38:11 -0300

    refactor(api): remove StreamingResponse model annotation and standardize type hints in chat routes

--- app/api/v1/routes/chat_router.py
+++ app/api/v1/routes/chat_router.py
@@ -1,21 +1,18 @@
-from fastapi import APIRouter, Depends, Body, status, HTTPException
-from typing import List
+from fastapi import APIRouter, Depends, status, HTTPException
 from app.models.user_model import User
-from app.models.chat_model import ChatMigrateRequest, ChatInputRequest, AIresponse, ChatResponse, Dialogue
+from app.models.chat_model import ChatMigrateRequest, ChatInputRequest, ChatResponse
 from app.chatbot.engine import ChatEngine
 from app.dependencies import get_engine, get_current_user, get_current_user_optional
 from app.database.postgres import get_session
 from app.repositories.chat_repository import find_chats_by_user_id
 from fastapi.responses import StreamingResponse
-
-
 from sqlmodel import Session
 from app.services import chat_service
 
-chat_router = APIRouter()
+router = APIRouter()
 
 
-@chat_router.post("/", response_model=StreamingResponse)
+@router.post("/")
 async def chat(req: ChatInputRequest, engine: ChatEngine = Depends(get_engine), 
                current_user : User | None = Depends(get_current_user_optional), 
                session: Session = Depends(get_session)) -> StreamingResponse:
@@ -27,7 +24,7 @@ async def chat(req: ChatInputRequest, engine: ChatEngine = Depends(get_engine),
     )
 
 
-@chat_router.post("/migrate", status_code=status.HTTP_201_CREATED)
+@router.post("/migrate", status_code=status.HTTP_201_CREATED)
 async def migrate(req: ChatMigrateRequest, 
                   session: Session = Depends(get_session),
                   current_user: User = Depends(get_current_user)):
@@ -43,9 +40,9 @@ async def migrate(req: ChatMigrateRequest,
        )
 
 
-@chat_router.get("/", response_model=list[ChatResponse])
+@router.get("/", response_model=list[ChatResponse])
 async def get_all_chats(session: Session = Depends(get_session),
-                        current_user: User = Depends(get_current_user)):
+                        current_user: User = Depends(get_current_user)) -> list[ChatResponse]:
     
     return find_chats_by_user_id(session, current_user.id)
     
